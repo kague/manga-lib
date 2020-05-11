9c356b97cfcc05e48514f46e3ca5683ac9612849
kague <yannickgirard@macbook-pro-de-yannick.home>
2020-05-11 23:34:00 +0200

    Updates

---
+++ precache-manifest.89b2eb1cacfc37da6ba0675e5102f0e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "276056862849a068de125c5ac600b699",
+    "url": "/manga-lib/index.html"
+  },
+  {
+    "revision": "d3d76f6a28631baa5458",
+    "url": "/manga-lib/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "43716e1bfc8388d984fd",
+    "url": "/manga-lib/static/js/2.e31b6823.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/manga-lib/static/js/2.e31b6823.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3d76f6a28631baa5458",
+    "url": "/manga-lib/static/js/main.cfc88c59.chunk.js"
+  },
+  {
+    "revision": "2b6febd943a0d7cff84e",
+    "url": "/manga-lib/static/js/runtime-main.417141bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/manga-lib/static/media/logo.5d5d9eef.svg"
+  }
+]);
